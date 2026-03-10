24c52f1bba03ee3196d2da9d6aa1ea3c4afd52bf
Peter Milne <peter.milne@d-tacq.com>
2026-03-10 11:37:20 +0000

    instantiate PM:BUF:xx using NELM=ACQ400_PM_NLW There is a certain element of hardcoding here. Of course if we set NELM to max on load, device support will set actual NORD  On branch HARDCODE_SSB_IGNORE_SOE_LUT 	modified:   db/pm_base.db 	new file:   db/pm_raw.db 	modified:   scripts/load.pm

--- scripts/load.pm
+++ scripts/load.pm
@@ -2,16 +2,22 @@
 
 SPORT=PM
 # ACQ400_FMT_SIM_SOURCE=WRTS
-
+ACQ400_PM_NBUF=${ACQ400_PM_NBUF:-20}
+# default : 1024 * 128 / 4 = 32768
+ACQ400_PM_NLW=${ACQ400_PM_NLW:-32768}
 
 echo acq400_PM_Configure\($SPORT,$SRC\)
 PRAMS="UUT=${IOC_HOST},PORT=$SPORT,ADDR=0,TIMEOUT=0"
 
-for PMB in $(seq 0 19); do
-        PMB02=$(printf %02d $PMB)
-        echo dbLoadRecords\(\"db/pm_base.db\",\"${PRAMS},PMB=${PMB},PMB02=${PMB02}\"\)
+echo dbLoadRecords\(\"db/pm_base.db\",\"${PRAMS},NBUF=${ACQ400_PM_NBUF}\"\)
+
+NBM1=$((ACQ400_PM_NBUF-1))
+# 00: most recent, 19: oldest of 20
+for PMBUF in $(seq 0 $NBM1); do
+        PMBUF02=$(printf %02d $PMBUF)
+        echo dbLoadRecords\(\"db/pm_raw.db\",\"${PRAMS},NELM=${ACQ400_PM_NLW},PMBUF=${PMBUF},PMBUF02=${PMBUF02}\"\)
 done
 
-[ -z $ACQ400_PM_NELM ] && ACQ400_PM_NELM=20
-echo dbLoadRecords\(\"db/pm_tab.db\",\"${PRAMS},PMB=${PMB},ROWS=${ACQ400_PM_NELM}\"\)
+ACQ400_PM_NELM=${ACQ400_PM_NELM:-20}
+echo dbLoadRecords\(\"db/pm_tab.db\",\"${PRAMS},ROWS=${ACQ400_PM_NELM}\"\)
 
