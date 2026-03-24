1103840aaf1fb032e447ce808ad6198f000b35db
Peter Milne <peter.milne@d-tacq.com>
2026-03-24 19:22:39 +0000

    xrm_ht_monitor is go .. json, kinda!  On branch main 	modified:   xrmIocApp/src/Makefile 	modified:   xrmIocApp/src/xrm_ht_monitor.cpp

--- xrmIocApp/src/xrm_ht_monitor.cpp
+++ xrmIocApp/src/xrm_ht_monitor.cpp
@@ -16,17 +16,117 @@ acq2206_588:SOE_HLD
     read them all into a binary array, output as HEADER + RAW pairs.
  */
 
+#include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
+#include "acq-util.h"
+#include "split2.h"
+#include "xrm_structs.h"
+
+
+
+int G_verbose = ::getenv_default("VERBOSE", 0);
+
+#define MAXSIZE 65536    // if pvxmonitor output ever greater than this per line, we're in trouble
+
+
+
+void print(SOE_HOLD_HEADER& header)
+{
+	printf("pv_id:		%u,\n", header.pv_id);
+	printf("client_data:	%u,\n", header.client_data);
+	printf("timestamp:	%llu,\n", header.timestamp);
+	printf("data_offset:	%u,\n", header.data_offset);
+	printf("ss_u32:		%u,\n", header.ss_u32);
+	printf("ai_count:	%u,\n", header.ai_count);
+	printf("di_count:	%u,\n", header.di_count);
+	printf("sp_count:	%u\n", header.sp_count);
+
+}
+void print(SOE_HOLD_HEADER& header, int* ht_data)
+{
+	short* ai16 = (short*)ht_data;
+	printf("ai16: [ ");
+	for (int ic = 0; ic < header.ai_count; ++ic){
+		printf("%d,", ai16[ic]);
+	}
+	printf(" ],\n");
+	printf("di32: [ ");
+	unsigned* di32 = (unsigned*)ht_data + header.ai_count/2;
+	for (int id = 0; id < header.di_count; ++id){
+		printf("0x%08x,", di32[id]);
+	}
+	printf(" ],\n");
+	printf("sp32: [ ");
+	unsigned* sp32 = di32 + header.di_count;
+	for (int isp = 0; isp < header.sp_count; ++isp){
+		printf("0x%08x,", sp32[isp]);
+	}
+	printf(" ]\n");
+}
+void parse(int* ht_data, unsigned nelems)
+{
+	if (G_verbose > 1 ) fprintf(stderr, "parse: %p, %u\n",
+			ht_data, nelems);
+	for (SOE_HOLD_HEADER* header = (SOE_HOLD_HEADER*)ht_data;
+			header->pv_id != 0; ++header){
+		print(*header);
+		print(*header, ht_data+header->data_offset);
+	}
+
+}
+
+#define NEEDLE "value int32_t[] = "
+
+void parse(char* txt)
+{
+	unsigned nelems;
+	if (G_verbose > 1 ) fprintf(stderr, "parse: \"%s\"\n", txt);
+
+	if (sscanf(txt, "{%u}", &nelems) != 1){
+		fprintf(stderr, "scan fail\n");
+		return;
+	}
+	char* start = strchr(txt, '[');
+	assert(start != 0);
+	char* end = strchr(txt, ']');
+	assert(end != 0);
+	*end = '\0';
+
+	VIS value_ints;
+	split2(++start, value_ints, ',');
+	if (G_verbose > 1 )
+		fprintf(stderr, "parse: value_ints.size %u nelems %u\n",
+				value_ints.size(), nelems);
+
+	assert(value_ints.size() == nelems);
+
+	int* ht_data = new int[nelems];
+	int* cursor = ht_data;
+	for (int ii: value_ints){
+		if (G_verbose > 2 ){
+			fprintf(stderr, "idx:%d ii:%d\n",
+					cursor - ht_data, ii);
+		}
+		*cursor++ = ii;
+		assert(cursor-ht_data <= nelems);
+	}
+	parse(ht_data, nelems);
+	delete [] ht_data;
+}
 int main(int argc, char* argv[]){
-	printf("Hello World\n");
-	char myline[512];
+	static char myline[MAXSIZE];
 	int lno = 0;
 
 	FILE *pp = popen("unbuffer pvxmonitor -r value  acq2206_588:SOE_HLD", "r");
-	while (fgets(myline, 512, pp) != 0){
+	while (fgets(myline, MAXSIZE, pp) != 0){
 		lno += 1;
-		printf("[%2d] %s", lno, myline);
+		if (G_verbose > 2 ) fprintf(stderr, "[%2d] %s\n", lno, myline);
+		char* cursor = strstr(myline, NEEDLE);
+		if (cursor){
+			parse(cursor+strlen(NEEDLE));
+		}
 	}
 	return 0;
 }
